e350d82aa44fbab9d2f5544f367b007c1c046bb5
IvanRomero03 <i.wells.ar@gmail.com>
2023-02-26 18:38:52 -0600

    vercel build stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,13 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel({
+    analytics: true,
+  }),
 });
